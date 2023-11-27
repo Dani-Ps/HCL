83efe9650f9115b5c77652e267163dd9e53f0434
Daniel Perez Serrano <daniperez439@gmail.com>
2023-11-27 17:55:35 +0100

    main completed

--- 1Trimestre/UD02T1/biblioteca.py
+++ 1Trimestre/UD02T1/biblioteca.py
@@ -1,10 +1,11 @@
-
 """Este módulo contiene la implementación de la clase Biblioteca."""
 import json
 from libro import Libro
 
+
 class Biblioteca:
     """Clase que representa una biblioteca."""
+
     def __init__(self):
         """Inicializa una instancia de la clase Biblioteca con una lista vacía de libros."""
         self.libros = []
@@ -24,7 +25,7 @@ class Biblioteca:
         return False
 
     def buscar_libro(self, isbn):
-        """Busca un libro en la biblioteca por su ISBN 
+        """Busca un libro en la biblioteca por su ISBN
         y devuelve el libro si lo encuentra, de lo contrario, devuelve None."""
         for libro in self.libros:
             if libro.isbn == isbn:
@@ -46,28 +47,32 @@ class Biblioteca:
 
     def guardar_en_archivo(self, nombre_archivo):
         """Guarda la información de los libros de la biblioteca en un archivo JSON."""
-        with open(nombre_archivo, 'w', encoding='utf-8') as archivo:
+        with open(nombre_archivo, "w", encoding="utf-8") as archivo:
             libros_serializados = []
             for libro in self.libros:
-                libros_serializados.append({
-                    'isbn': libro.isbn,
-                    'titulo': libro.titulo,
-                    'autor': libro.autor,
-                    'anio_publicacion': libro.anio_publicacion
-                })
+                libros_serializados.append(
+                    {
+                        "isbn": libro.isbn,
+                        "titulo": libro.titulo,
+                        "autor": libro.autor,
+                        "anio_publicacion": libro.anio_publicacion,
+                    }
+                )
             json.dump(libros_serializados, archivo)
 
     def cargar_desde_archivo(self, nombre_archivo):
         """Carga la información de los libros de un archivo JSON a la biblioteca."""
         try:
-            with open(nombre_archivo, 'r', encoding='utf-8') as archivo:
+            with open(nombre_archivo, "r", encoding="utf-8") as archivo:
                 libros_serializados = json.load(archivo)
                 for libro_serializado in libros_serializados:
-                    self.libros.append(Libro(
-                        libro_serializado['isbn'],
-                        libro_serializado['titulo'],
-                        libro_serializado['autor'],
-                        libro_serializado['anio_publicacion']
-                    ))
+                    self.libros.append(
+                        Libro(
+                            libro_serializado["isbn"],
+                            libro_serializado["titulo"],
+                            libro_serializado["autor"],
+                            libro_serializado["anio_publicacion"],
+                        )
+                    )
         except FileNotFoundError:
             print("El archivo no existe. Creando una nueva biblioteca.")
--- 1Trimestre/UD02T1/main.py
+++ 1Trimestre/UD02T1/main.py
@@ -1,4 +1,4 @@
-"""Este módulo contiene la implementación de la clase main."""
+
 from biblioteca import Biblioteca
 from libro import Libro
 
@@ -17,15 +17,51 @@ if __name__ == "__main__":
 
         opcion = input("Ingrese el número de la operación que desea realizar: ")
 
-        if opcion == "1":
-            isbn = input("Ingrese el ISBN del libro: ")
-            titulo = input("Ingrese el título del libro: ")
-            autor = input("Ingrese el autor del libro: ")
-            anio_publicacion = input("Ingrese el año de publicación del libro: ")
-            libro = Libro(isbn, titulo, autor, anio_publicacion)
-            biblioteca.agregar_libro(libro)
-            print("Libro agregado exitosamente.")
+        try:
+            opcion = int(opcion)  # Convertir la entrada a un número entero
 
-        # Resto de las opciones del menú
-        else:
-            print("Opción inválida. Por favor, ingrese un número del 1 al 6.")
+            if opcion == 1:
+                isbn = input("Ingrese el ISBN del libro: ")
+                titulo = input("Ingrese el título del libro: ")
+                autor = input("Ingrese el autor del libro: ")
+                anio_publicacion = input("Ingrese el año de publicación del libro: ")
+                libro = Libro(isbn, titulo, autor, anio_publicacion)
+                biblioteca.agregar_libro(libro)
+
+            elif opcion == 2:
+                isbn = input("Ingrese el ISBN del libro que desea actualizar: ")
+                titulo = input("Ingrese el nuevo título del libro: ")
+                autor = input("Ingrese el nuevo autor del libro: ")
+                anio_publicacion = input("Ingrese el nuevo año de publicación del libro: ")
+                if biblioteca.actualizar_libro(isbn, titulo, autor, anio_publicacion):
+                    print("Libro actualizado exitosamente.")
+                else:
+                    print("ISBN no encontrado. No se pudo actualizar el libro.")
+
+            elif opcion == 3:
+                isbn = input("Ingrese el ISBN del libro que desea buscar: ")
+                libro = biblioteca.buscar_libro(isbn)
+                if libro:
+                    libro.mostrar_info()
+                else:
+                    print("ISBN no encontrado. No se pudo encontrar el libro.")
+
+            elif opcion == 4:
+                isbn = input("Ingrese el ISBN del libro que desea eliminar: ")
+                if biblioteca.eliminar_libro(isbn):
+                    print("Libro eliminado exitosamente.")
+                else:
+                    print("ISBN no encontrado. No se pudo eliminar el libro.")
+
+            elif opcion == 5:
+                biblioteca.mostrar_libros()
+
+            elif opcion == 6:
+                biblioteca.guardar_en_archivo("biblioteca.json")
+                break
+
+            else:
+                print("Opción inválida. Por favor, ingrese un número del 1 al 6.")
+
+        except ValueError:
+            print("Por favor, ingrese un número válido.")
